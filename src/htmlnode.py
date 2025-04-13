ccdab174d767507690a94f2295cf7dc158b71269
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-13 13:33:48 -0500

    feat: Add HTMLNode class

---
+++ src/htmlnode.py
@@ -0,0 +1,18 @@
+class HTMLNode:
+  def __init__(self, tag=None, value=None, children=None, props=None):
+    self.tag = tag
+    self.value = value
+    self.children = children
+    self.props = props
+  
+  def to_html(self):
+    raise NotImplementedError()
+  
+  def props_to_html(self):
+    html_string = ''
+    for key in self.props:
+      html_string = html_string + f" {key}=\"{self.props[key]}\""
+    return html_string + ' '
+  
+  def __repr__(self):
+    return f"HTMLNode | tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props},"
