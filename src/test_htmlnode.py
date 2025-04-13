1b7860d676c7f04e7a2e03c7845a62479ab87646
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-13 13:34:10 -0500

    test: Add HTMLNode tests

---
+++ src/test_htmlnode.py
@@ -0,0 +1,17 @@
+import unittest
+
+from htmlnode import (
+  HTMLNode,
+)
+
+class TestTextNode(unittest.TestCase):
+  def test_eq(self):
+    linknode = HTMLNode("a", "A link to Google.com", None, {"href": "https://www.google.com", "target": "_blank",})
+    self.assertEqual(linknode.tag, "a")
+    self.assertIn("href=\"https://www.google.com\"", linknode.props_to_html())
+    self.assertIn("target=\"_blank\"", linknode.props_to_html())
+    self.assertEqual(linknode.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\" ")
+    self.assertRaises(NotImplementedError, linknode.to_html)
+
+if __name__ == "__main__":
+  unittest.main()
