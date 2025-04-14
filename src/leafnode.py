f0de1f08321fb158ea2d86722109b918e55b465a
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-14 11:24:31 -0500

    fix: Move required value to to_html method and ignore if tag = img

--- src/leafnode.py
+++ src/leafnode.py
@@ -3,10 +3,10 @@ from htmlnode import HTMLNode
 class LeafNode(HTMLNode):
   def __init__(self, tag, value, props=None):
     super().__init__(tag, value, None, props)
-    if not value:
-      raise ValueError("Leaf nodes must have a value")
 
   def to_html(self):
+    if not self.value and (not self.tag == "img"):
+      raise ValueError("Leaf nodes must have a value")
     if not self.tag:
       return self.value
     props = self.props_to_html()
