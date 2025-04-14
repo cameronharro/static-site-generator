65866ecfb00616fcaa23d208f3cd72b1b1d6c5e8
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-14 09:32:52 -0500

    fix: remove trailing space in props_to_html method and test

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -10,9 +10,10 @@ class HTMLNode:
   
   def props_to_html(self):
     html_string = ''
-    for key in self.props:
-      html_string = html_string + f" {key}=\"{self.props[key]}\""
-    return html_string + ' '
+    if self.props:
+      for key in self.props:
+        html_string = html_string + f" {key}=\"{self.props[key]}\""
+    return html_string
   
   def __repr__(self):
     return f"HTMLNode | tag: {self.tag}, value: {self.value}, children: {self.children}, props: {self.props},"
--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -5,13 +5,13 @@ from htmlnode import (
 )
 
 class TestHTMLNode(unittest.TestCase):
-  def test_eq(self):
-    linknode = HTMLNode("a", "A link to Google.com", None, {"href": "https://www.google.com", "target": "_blank",})
-    self.assertEqual(linknode.tag, "a")
-    self.assertIn("href=\"https://www.google.com\"", linknode.props_to_html())
-    self.assertIn("target=\"_blank\"", linknode.props_to_html())
-    self.assertEqual(linknode.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\" ")
-    self.assertRaises(NotImplementedError, linknode.to_html)
+  def test(self):
+    link_node = HTMLNode("a", "A link to Google.com", None, {"href": "https://www.google.com", "target": "_blank",})
+    self.assertEqual(link_node.tag, "a")
+    self.assertIn("href=\"https://www.google.com\"", link_node.props_to_html())
+    self.assertIn("target=\"_blank\"", link_node.props_to_html())
+    self.assertEqual(link_node.props_to_html(), " href=\"https://www.google.com\" target=\"_blank\"")
+    self.assertRaises(NotImplementedError, link_node.to_html)
 
 if __name__ == "__main__":
   unittest.main()
