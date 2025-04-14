97e3c7c9babb1ed298e178daef099ac3bc7d2954
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-14 11:26:09 -0500

    test: Accept changed validation behavior

--- src/test_leafnode.py
+++ src/test_leafnode.py
@@ -9,7 +9,7 @@ class TestLeafNode(unittest.TestCase):
     self.assertEqual(hello_node.to_html(), "<p>Hello, world!</p>")
     link_node = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
     self.assertEqual(link_node.to_html(), "<a href=\"https://www.google.com\">Click me!</a>")
-    self.assertRaises(ValueError, LeafNode, None, None)
+    self.assertRaises(ValueError, LeafNode(None, None).to_html,)
     self.assertEqual(LeafNode(None, "Some text").to_html(), "Some text")
 
 if __name__ == "__main__":
