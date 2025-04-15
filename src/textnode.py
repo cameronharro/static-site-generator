0778100fbca34d123bec50180bb365716504315b
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-15 16:19:16 -0500

    chore: Use text_type.name when representing TextNode

--- src/textnode.py
+++ src/textnode.py
@@ -26,4 +26,4 @@ class TextNode:
       and self.url == other.url
     )
   def __repr__(self):
-    return f"TextNode({self.text}, {self.text_type.value}, {self.url})"
+    return f"TextNode({self.text}, {self.text_type.name}, {self.url})"
