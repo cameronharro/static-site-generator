2cd68b8bb31bbd3d0195ca07a9b53bfd998a2e73
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-16 10:48:04 -0500

    refactor: Make split_nodes_delimiter higher order Allows one-time configuration of split_nodes bold/italic/code for better re-use.

--- src/split_nodes.py
+++ src/split_nodes.py
@@ -5,19 +5,25 @@ from textnode import (
   TextNode
 )
 
-def split_nodes_delimiter(old_nodes, delimiter, text_type):
-  if not isinstance(text_type, TextType):
-    raise ValueError("text_type must be an instance of the TextType enum")
-  new_nodes = []
-  for node in old_nodes:
-    text = node.text
-    parent_type = node.text_type
-    split_text = text.split(delimiter)
-    for i in range(0, len(split_text)):
-      snippet = split_text[i]
-      if len(snippet) > 0:
-        new_nodes.append(TextNode(snippet, parent_type if i % 2 == 0 else text_type))
-  return new_nodes
+def split_nodes_delimiter(delimiter, text_type):
+  def inner_func(old_nodes):
+    if not isinstance(text_type, TextType):
+      raise ValueError("text_type must be an instance of the TextType enum")
+    new_nodes = []
+    for node in old_nodes:
+      text = node.text
+      parent_type = node.text_type
+      split_text = text.split(delimiter)
+      for i in range(0, len(split_text)):
+        snippet = split_text[i]
+        if len(snippet) > 0:
+          new_nodes.append(TextNode(snippet, parent_type if i % 2 == 0 else text_type))
+    return new_nodes
+  return inner_func
+
+split_nodes_bold = split_nodes_delimiter("**", TextType.BOLD)
+split_nodes_italic = split_nodes_delimiter("_", TextType.ITALIC)
+split_nodes_code = split_nodes_delimiter("`", TextType.CODE)
 
 value_re = r"\[[\w\ ]*\]"
 url_re = r"\(https:\/\/[\w\.\/\@]*\)"
