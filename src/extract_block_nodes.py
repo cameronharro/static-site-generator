99abb02d184a6f5fd726c381204fd13649486891
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-16 14:15:31 -0500

    feat: Add BlockType enum + heading eval and tests

--- src/extract_block_nodes.py
+++ src/extract_block_nodes.py
@@ -1,3 +1,30 @@
+from enum import Enum
+import re
+
+BlockType = Enum(
+  'BlockType',
+  [
+    "PARAGRAPH",
+    "HEADING",
+    "CODE",
+    "QUOTE",
+    "UNORDERED_LIST",
+    "ORDERED_LIST"
+  ]
+)
+
+h_re = r"#{1,6}\ .*(?!\n)"
+code_re = r"```.*```"
+quote_line_re = r"\n>.*"
+ul_line_re = r"\n-.*"
+ol_line_re = r"\n(\d+)\..*"
+
 def markdown_to_blocks(markdown):
   blocks = markdown.split("\n\n")
   return list(map(lambda x: x.strip("\n "), blocks))
+
+def block_to_block_type(block):
+  heading = re.match(h_re, block)
+  if heading:
+    return BlockType.HEADING
+  return BlockType.PARAGRAPH
--- src/test_extract_block_markdown.py
+++ src/test_extract_block_markdown.py
@@ -1,7 +1,9 @@
 import unittest
 
 from extract_block_nodes import (
-   markdown_to_blocks
+   markdown_to_blocks,
+   block_to_block_type,
+   BlockType,
 )
 
 class TestExtractBlocks(unittest.TestCase):
@@ -64,3 +66,38 @@ This is the same paragraph on a new line
         "This is another paragraph with _italic_ text and `code` here\nThis is the same paragraph on a new line\n- This is a list\n- with items",
       ],
     )
+
+class TestBlockToHeading(unittest.TestCase):
+  def test_heading_1(self):
+    block = "# Heading 1"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_heading_2(self):
+    block = "## Heading 2"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_heading_3(self):
+    block = "### Heading 3"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_heading_4(self):
+    block = "#### Heading 4"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_heading_5(self):
+    block = "##### Heading 5"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_heading_6(self):
+    block = "###### Heading 6"
+    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+  def test_consecutive_headings(self):
+    block = """
+  # Heading 1
+  # Heading 2
+  """
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+  def test_unstripped_newline(self):
+    block = """
+  # Heading 1
+
+  """
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+  def test_no_space(self):
+    block = "#Heading"
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
