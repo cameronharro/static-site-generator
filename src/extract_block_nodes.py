a8cfdf4bb6dea68be511e94750904f7406ea71e3
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-23 16:16:33 -0500

    feat: Make BlockType and functions output html tag name

--- src/extract_block_nodes.py
+++ src/extract_block_nodes.py
@@ -1,17 +1,13 @@
 from enum import Enum
 import re
 
-BlockType = Enum(
-  'BlockType',
-  [
-    "PARAGRAPH",
-    "HEADING",
-    "CODE",
-    "QUOTE",
-    "UNORDERED_LIST",
-    "ORDERED_LIST"
-  ]
-)
+class BlockType(Enum):
+  PARAGRAPH = "p"
+  HEADING = "h"
+  CODE = "code"
+  QUOTE = "blockquote"
+  UNORDERED_LIST = "ul"
+  ORDERED_LIST = "ol"
 
 h_re = r"#{1,6}\ .*(?!\n)"
 code_re = r"^```(.|[\n])*```$"
@@ -26,21 +22,26 @@ def markdown_to_blocks(markdown):
 def block_to_block_type(block):
   heading = re.match(h_re, block)
   if heading:
-    return BlockType.HEADING
+    return BlockType.HEADING.value + f"{get_heading_level(block)}"
   code = re.match(code_re, block)
   if code:
-    return BlockType.CODE
+    return BlockType.CODE.value
   new_lines = re.findall(r"\n", block)
   quote = re.findall(quote_line_re, block, re.M)
   if len(quote) == len(new_lines) + 1:
-    return BlockType.QUOTE
+    return BlockType.QUOTE.value
   ul = re.findall(ul_line_re, block, re.M)
   if len(ul) == len(new_lines) + 1:
-    return BlockType.UNORDERED_LIST  
+    return BlockType.UNORDERED_LIST  .value
   ol = re.findall(ol_line_re, block, re.M)
   if len(ol) == len(new_lines) + 1:
     numbers = list(map(lambda x: int(x), ol))
     target = list(range(1, len(ol) + 1))
     if numbers == target:
-      return BlockType.ORDERED_LIST  
-  return BlockType.PARAGRAPH
+      return BlockType.ORDERED_LIST.value
+  return BlockType.PARAGRAPH.value
+
+def get_heading_level(text):
+  if text[0] != "#":
+    return 0
+  return 1 + get_heading_level(text[1:])
--- src/test_extract_block_markdown.py
+++ src/test_extract_block_markdown.py
@@ -71,42 +71,42 @@ This is the same paragraph on a new line
 class TestBlockToHeading(unittest.TestCase):
   def test_heading_1(self):
     block = "# Heading 1"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h1")
   def test_heading_2(self):
     block = "## Heading 2"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h2")
   def test_heading_3(self):
     block = "### Heading 3"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h3")
   def test_heading_4(self):
     block = "#### Heading 4"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h4")
   def test_heading_5(self):
     block = "##### Heading 5"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h5")
   def test_heading_6(self):
     block = "###### Heading 6"
-    self.assertEqual(block_to_block_type(block), BlockType.HEADING)
+    self.assertEqual(block_to_block_type(block), "h6")
   def test_consecutive_headings(self):
     block = """
   # Heading 1
   # Heading 2
   """
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_unstripped_newline(self):
     block = """
   # Heading 1
 
   """
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_no_space(self):
     block = "#Heading"
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
 
 class TestBlockToCode(unittest.TestCase):
   def test_code(self):
     block = "```Here is some single-line code```"
-    self.assertEqual(block_to_block_type(block), BlockType.CODE)
+    self.assertEqual(block_to_block_type(block), BlockType.CODE.value)
 
   def test_multiline(self):
     block = """```
@@ -115,7 +115,7 @@ It's multiline
 {It has braces}
 and other stuff :/?%
 ```"""
-    self.assertEqual(block_to_block_type(block), BlockType.CODE)
+    self.assertEqual(block_to_block_type(block), BlockType.CODE.value)
   def test_un_stripped(self):
     block = """
     ```
@@ -124,7 +124,7 @@ It's multiline
 {It has braces}
 and other stuff :/?%
 ```"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_not_divided(self):
     block = """
     ```
@@ -134,71 +134,71 @@ It's multiline
 and other stuff :/?%
 ```
 but it has trailing text"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
 
 class TestBlockToQuote(unittest.TestCase):
   def test_single_line(self):
     block = ">A quote"
-    self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+    self.assertEqual(block_to_block_type(block), BlockType.QUOTE.value)
   def test_multi_line(self):
     block = """>A quote
 >A second quote"""
-    self.assertEqual(block_to_block_type(block), BlockType.QUOTE)
+    self.assertEqual(block_to_block_type(block), BlockType.QUOTE.value)
   def test_unstripped(self):
     block = """
 >A quote
 >A second quote"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_wrong_markdown(self):
     block = """>A quote
 -A second quote
 >A third quote"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
 
 class TestBlockToUL(unittest.TestCase):
   def test_single_line(self):
     block = "- A list item"
-    self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+    self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST.value)
   def test_multi_line(self):
     block = """- A list item
 - A second list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST)
+    self.assertEqual(block_to_block_type(block), BlockType.UNORDERED_LIST.value)
   def test_unstripped(self):
     block = """
 - A list item
 - A second list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_wrong_markdown(self):
     block = """- A list item
 > A second list item
 - A third list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_more_wrong_markdown(self):
     block = """-A list item
 - A second list item
 - A third list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
 
 class TestBlockToOL(unittest.TestCase):
   def test_single_line(self):
     block = "1. A list item"
-    self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+    self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST.value)
   def test_multi_line(self):
     block = """1. A list item
 2. A second list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST)
+    self.assertEqual(block_to_block_type(block), BlockType.ORDERED_LIST.value)
   def test_unstripped(self):
     block = """
 1. A list item
 2. A second list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_wrong_markdown(self):
     block = """1. A list item
 3. A second list item
 3. A third list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
   def test_more_wrong_markdown(self):
     block = """1.A list item
 2. A second list item
 3. A third list item"""
-    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH)
+    self.assertEqual(block_to_block_type(block), BlockType.PARAGRAPH.value)
