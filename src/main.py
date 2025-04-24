743db3ed28c8cc810e0ca92d9e464772b4cfa43e
cameronharro <103271364+cameronharro@users.noreply.github.com>
2025-04-23 22:21:58 -0500

    feat: Add generate_page function

--- src/main.py
+++ src/main.py
@@ -12,6 +12,11 @@ from shutil import (
   rmtree,
 )
 
+from markdown_to_html import(
+  extract_title,
+  markdown_to_html_node
+)
+
 def matching_dest_path(dest_root, path):
   return Path.joinpath(dest_root, *path.parts[1:])
 
@@ -43,8 +48,28 @@ def get_paths_recursive(curr_path):
       dirs.extend(nested_dirs)
   return files, dirs
 
+def generate_page(from_path, template_path, dest_path):
+  print(f"Generating page from {from_path} to {dest_path} using {template_path}")
+  src_file = open(from_path)
+  md = src_file.read()
+  src_file.close()
+
+  template_file = open(template_path)
+  html = template_file.read()
+  template_file.close()
+
+  title = extract_title(md)
+  content = markdown_to_html_node(md).to_html()
+  html = html.replace("{{ Title }}", title)
+  html = html.replace("{{ Content }}", content)
+  
+  dest_file = open(dest_path, "x")
+  dest_file.write(html)
+  dest_file.close()
+
 def main():
   clean_copy_directory_contents("./static", "./public")
+  generate_page("./content/index.md", "./template.html", "./public/index.html")
 
 if __name__ == "__main__":
   main()
